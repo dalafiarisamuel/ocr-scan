b5a228077a89d625f6dd0d4e2c297ff27caf0eef
Dalafiari Samuel <dalafiarisamuel@gmail.com>
2021-02-11 00:48:54 +0100

    migrated app to single page activity. project now has navigation component with multiple fragments

--- app/src/main/java/ng/mint/ocrscanner/views/activities/MainActivity.kt
+++ app/src/main/java/ng/mint/ocrscanner/views/activities/MainActivity.kt
@@ -1,23 +1,19 @@
 package ng.mint.ocrscanner.views.activities
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
 import ng.mint.ocrscanner.R
-import ng.mint.ocrscanner.callbacks.ProcessCardDetail
 import ng.mint.ocrscanner.database.Database
 import ng.mint.ocrscanner.databinding.ActivityMainBinding
-import ng.mint.ocrscanner.model.CardResult
 import ng.mint.ocrscanner.viewmodel.CardViewModelFactory
 import ng.mint.ocrscanner.viewmodel.CardsRepository
 import ng.mint.ocrscanner.viewmodel.CardsViewModel
-import ng.mint.ocrscanner.views.mvc.MainActivityViewsMvc
 
 
-class MainActivity : BaseActivity(), ProcessCardDetail {
+class MainActivity : BaseActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var view: MainActivityViewsMvc
     private val repository: CardsRepository by lazy {
         CardsRepository(Database.getInstance(this))
     }
@@ -25,53 +21,27 @@ class MainActivity : BaseActivity(), ProcessCardDetail {
     private val viewModelFactory: CardViewModelFactory by lazy {
         CardViewModelFactory(application, repository)
     }
-    private val viewModel: CardsViewModel by lazy {
-        ViewModelProvider(this, viewModelFactory).get(
-            CardsViewModel::class.java
-        )
-    }
-
+    private lateinit var viewModel: CardsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestPermissionsFromDevice()
 
+        viewModel = ViewModelProvider(this, viewModelFactory).get(
+            CardsViewModel::class.java
+        )
+
+
         binding = ActivityMainBinding.inflate(layoutInflater)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
         setContentView(binding.root)
-        view = MainActivityViewsMvc(this, binding, this)
         setLightStatusBar(binding.root)
-        viewModel.data.observe(this, this::observeData)
-    }
 
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        view.onActivityResult(requestCode = requestCode, resultCode = resultCode, data = data)
-    }
-
-    private fun observeData(cardResult: CardResult) {
-        progressDialog.dismissDialog()
-        if (cardResult is CardResult.Failure) {
-            messageDialog.displayMessage(getString(R.string.no_bin_number_was_found))
-        }
-        view.updateValue(cardResult)
 
     }
-
-    override fun processCard(value: String) {
-
-        when (internetConnection.isConnectingToInternet()) {
-            true -> {
-                progressDialog.showLoading(getString(R.string.processing))
-                viewModel.processCardDetail(value)
-            }
-
-            false -> messageDialog.displayMessage(getString(R.string.no_internet_connection))
-
-        }
-
-    }
-
 
 }
 
---
+++ app/src/main/java/ng/mint/ocrscanner/views/fragments/CardInformationFragment.kt
@@ -0,0 +1,165 @@
+package ng.mint.ocrscanner.views.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import io.card.payment.CardIOActivity
+import io.card.payment.CreditCard
+import ng.mint.ocrscanner.R
+import ng.mint.ocrscanner.database.Database
+import ng.mint.ocrscanner.databinding.FragmentCardInformationBinding
+import ng.mint.ocrscanner.model.CardResult
+import ng.mint.ocrscanner.networking.ConnectionDetector
+import ng.mint.ocrscanner.viewmodel.CardViewModelFactory
+import ng.mint.ocrscanner.viewmodel.CardsRepository
+import ng.mint.ocrscanner.viewmodel.CardsViewModel
+import ng.mint.ocrscanner.views.activities.BaseActivity
+import ng.mint.ocrscanner.views.common.MessageDialogManager
+import ng.mint.ocrscanner.views.common.ProgressDialogManager
+
+class CardInformationFragment : Fragment(R.layout.fragment_card_information) {
+
+    companion object {
+
+        const val MY_SCAN_REQUEST_CODE = 500
+    }
+
+    private var binding: FragmentCardInformationBinding? = null
+    private lateinit var activity: BaseActivity
+
+    private lateinit var progressDialog: ProgressDialogManager
+    private lateinit var messageDialog: MessageDialogManager
+    private lateinit var internetConnection: ConnectionDetector
+    private lateinit var viewModel: CardsViewModel
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bind = FragmentCardInformationBinding.bind(view)
+        binding = bind
+
+        activity = getActivity() as BaseActivity
+
+        progressDialog = activity.progressDialog
+        messageDialog = activity.messageDialog
+        internetConnection = activity.internetConnection
+
+
+        binding?.panInputField?.doAfterTextChanged {
+
+            binding?.cardInformationLayout?.visibility = View.GONE
+        }
+
+        binding?.nextButton?.setOnClickListener {
+            activity.hideKeyboard()
+            val panNumber = binding?.panInputField?.text?.toString() ?: ""
+            if (panNumber.length > 5) {
+                processCard(panNumber.take(8))
+            }
+        }
+
+        binding?.scanButton?.setOnClickListener(this::onScanClicked)
+
+        binding?.recentCards?.setOnClickListener {
+
+            it.findNavController()
+                .navigate(R.id.action_cardInformationFragment_to_recentlyViewedCardsFragment)
+
+        }
+
+        val repository = CardsRepository(Database.getInstance(activity))
+
+        val viewModelFactory =
+            CardViewModelFactory(activity.application, repository)
+
+
+        viewModel = ViewModelProvider(this, viewModelFactory).get(
+            CardsViewModel::class.java
+        )
+
+        viewModel.data.observe(viewLifecycleOwner, this::observeData)
+
+    }
+
+    private fun onCardActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == MY_SCAN_REQUEST_CODE && data != null) {
+            val scanResult =
+                data.getParcelableExtra(CardIOActivity.EXTRA_SCAN_RESULT) as? CreditCard
+            scanResult?.run {
+                val panNumber = this.formattedCardNumber?.replace(" ", "") ?: ""
+                if (panNumber.length > 5) {
+                    processCard(panNumber)
+                }
+            }
+        }
+
+    }
+
+    private fun observeData(cardResult: CardResult) {
+        progressDialog.dismissDialog()
+        if (cardResult is CardResult.Failure) {
+            messageDialog.displayMessage(getString(R.string.no_bin_number_was_found))
+        }
+        updateValue(cardResult)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        onCardActivityResult(requestCode = requestCode, resultCode = resultCode, data = data)
+    }
+
+    private fun onScanClicked(view: View) {
+
+        val scanIntent = Intent(activity, CardIOActivity::class.java)
+        // customize these values to suit your needs.
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, false) // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, false) // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_POSTAL_CODE, false) // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_SUPPRESS_MANUAL_ENTRY, true) // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_HIDE_CARDIO_LOGO, true) // default: false
+
+        startActivityForResult(scanIntent, MY_SCAN_REQUEST_CODE)
+    }
+
+    private fun updateValue(cardResult: CardResult) {
+        when (cardResult) {
+            is CardResult.Success -> {
+                binding?.bankData = cardResult.data
+            }
+            is CardResult.Failure -> {
+                binding?.cardInformationLayout?.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun processCard(value: String) {
+
+        when (internetConnection.isConnectingToInternet()) {
+            true -> {
+                progressDialog.showLoading(getString(R.string.processing))
+                viewModel.processCardDetail(value)
+            }
+
+            false -> messageDialog.displayMessage(getString(R.string.no_internet_connection))
+
+        }
+
+    }
+
+
+    override fun onDestroyView() {
+        // Consider not storing the binding instance in a field
+        // if not needed.
+        binding = null
+        super.onDestroyView()
+    }
+
+
+}
