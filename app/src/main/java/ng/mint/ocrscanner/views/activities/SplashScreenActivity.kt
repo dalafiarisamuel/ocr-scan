69aca7dfdc709125b4714015d3e7ec36cb267ac5
Dalafiari Samuel <dalafiarisamuel@gmail.com>
2021-03-29 15:50:21 +0100

    implement splashscreen activity the right way

---
+++ app/src/main/java/ng/mint/ocrscanner/views/activities/SplashScreenActivity.kt
@@ -0,0 +1,15 @@
+package ng.mint.ocrscanner.views.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
