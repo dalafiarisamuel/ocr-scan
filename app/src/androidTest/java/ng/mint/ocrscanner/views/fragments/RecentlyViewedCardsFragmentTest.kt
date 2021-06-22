e5a3554c3a81b156f97b4cefd1af22573495cec2
Dalafiari Samuel <dalafiarisamuel@gmail.com>
2021-06-22 08:35:18 +0100

    added swipe to delete action in recently viewed cards list fragment

--- app/src/androidTest/java/ng/mint/ocrscanner/views/fragments/RecentlyViewedCardsFragmentTest.kt
+++ app/src/androidTest/java/ng/mint/ocrscanner/views/fragments/RecentlyViewedCardsFragmentTest.kt
@@ -9,15 +9,19 @@ import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import ng.mint.ocrscanner.R
 import ng.mint.ocrscanner.adapters.CustomBindAdapter.setData
 import ng.mint.ocrscanner.adapters.RecentCardsAdapter
+import ng.mint.ocrscanner.getOrAwaitValue
 import ng.mint.ocrscanner.launchFragmentInHiltContainer
 import ng.mint.ocrscanner.model.RecentCard
 import ng.mint.ocrscanner.model.RecentCardsState
+import ng.mint.ocrscanner.viewmodel.CardsViewModel
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -108,4 +112,48 @@ class RecentlyViewedCardsFragmentTest {
         ))
 
     }
+
+    /*@Test
+    fun onRecentCardSwipeLeft_deleteItem() = runBlockingTest {
+
+        var viewModelTest: CardsViewModel? = null
+
+        val recentCard = RecentCard(
+            bin = "524523",
+            dateCreated = "10-12-2020",
+            bank = "GT Bank",
+            currency = "NGN",
+            country = "Nigeria",
+            phone = "08141289822",
+            emoji = "🇳🇬",
+            scheme = "Master",
+            type = "Credit",
+            id = 1
+        )
+
+        val navController = mock(NavController::class.java)
+        launchFragmentInHiltContainer<RecentlyViewedCardsFragment> {
+
+            Navigation.setViewNavController(requireView(), navController)
+
+            binding.savedRecyclerview.setData(
+                RecentCardsState.RecentCardList(
+                    mutableListOf(recentCard)
+                ), dataHandler
+            )
+
+            viewModelTest = viewModel
+        }
+
+        onView(withId(R.id.saved_recyclerview)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecentCardsAdapter.RecentCardHolder>(
+                0,
+                ViewActions.swipeLeft()
+            )
+        )
+
+        val list = viewModelTest?.getRecentCardDataListLiveData()?.getOrAwaitValue()
+        assertThat(list).isEmpty()
+
+    }*/
 }
